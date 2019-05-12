b4d369059eed0bb0cd2cfa2dd756d565ca5a2fd1
Dennis Chung <chungdennis6@gmail.com>
2019-05-12 03:17:59 +0000

    modified cli.rb

--- lib/cars_under_20k.rb
+++ lib/cars_under_20k.rb
@@ -1,2 +1,2 @@
 require_relative "./cars_under_20k/version"
-require_relative './cli'
+require_relative './cli.rb'
--- lib/cli.rb
+++ lib/cli.rb
@@ -2,8 +2,24 @@
 class CarsUnder20k::CLI
   
   def call
-    
     puts "Cars Under 20k:"
+    list_cars
+    menu
+  end
+  
+  def list_cars
+    puts "Top 5 Cars Under 20k"
+    puts <<-DOC
+    1. Hyundai Accent – Base Price: $15,915
+    2. Toyota Yaris Sedan – Base Price: $16,380
+    3. Subaru Impreza 5-Door – Base Price: $19,980
+    4. Volkswagen Jetta – Base Price: $19,640
+    5. Chevrolet Cruze Sedan – Base Price: $18,870
+    DOC
+  end
+  
+  def menu
+    puts "Enter the number of the car you would like more info on:"
   end
   
 end
